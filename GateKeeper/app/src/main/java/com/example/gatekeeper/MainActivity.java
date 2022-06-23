956b6a1e3c409d52a45865301bfc21421a8c3cef
arthurggp <arthur_ggp@hotmail.com>
2022-06-23 14:23:11 -0300

    INTEGRAÇÃO BANCO

--- GateKeeper/app/src/main/java/com/example/gatekeeper/MainActivity.java
+++ GateKeeper/app/src/main/java/com/example/gatekeeper/MainActivity.java
@@ -43,7 +43,7 @@ public class MainActivity extends AppCompatActivity {
                 public void onClick(DialogInterface dialogInterface, int i) {
                     dialogInterface.dismiss();
                 }
-            });
+            }).show();
         }
     });
 
---
+++ GateKeeper/app/src/main/java/com/example/gatekeeper/models/ConvidadoModel.java
@@ -0,0 +1,57 @@
+package com.example.gatekeeper.models;
+
+public class ConvidadoModel {
+    String id       = "";
+    String cpf      = "";
+    String rg       = "";
+    String nome     = "";
+    String status   = "";
+
+    public ConvidadoModel(String id,String cpf,String rg,String nome,String status){
+        this.id = id;
+        this.cpf = cpf;
+        this.rg = rg;
+        this.nome = nome;
+        this.status = status;
+    }
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public String getCpf() {
+        return cpf;
+    }
+
+    public void setCpf(String cpf) {
+        this.cpf = cpf;
+    }
+
+    public String getRg() {
+        return rg;
+    }
+
+    public void setRg(String rg) {
+        this.rg = rg;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public String getStatus() {
+        return status;
+    }
+
+    public void setStatus(String status) {
+        this.status = status;
+    }
+}
