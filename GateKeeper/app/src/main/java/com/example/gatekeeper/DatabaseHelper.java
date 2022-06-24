98aff862bbb268d25344b8c00ffc2dd40b1bd2cd
arthurggp <arthur_ggp@hotmail.com>
2022-06-23 21:07:09 -0300

    telas de inserção de dados

---
+++ GateKeeper/app/src/main/java/com/example/gatekeeper/DatabaseHelper.java
@@ -0,0 +1,100 @@
+package com.example.gatekeeper;
+
+import android.content.ContentValues;
+import android.content.Context;
+import android.database.Cursor;
+import android.database.sqlite.SQLiteDatabase;
+import android.database.sqlite.SQLiteOpenHelper;
+
+import androidx.annotation.Nullable;
+
+import com.example.gatekeeper.models.ConvidadoModel;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class DatabaseHelper extends SQLiteOpenHelper {
+
+    private static final int DATABASE_VERSION=1;
+    private static final String NOME_BANCO = "gatekeeper.db";
+    private static final String NOME_TABELA = "convidados";
+    private static final String ID = "id";
+    private static final String CPF = "cpf";
+    private static final String RG = "rg";
+    private static final String NOME = "nome";
+    private static final String STATUS = "status";
+
+    public DatabaseHelper(Context context) {
+        super(context,NOME_BANCO,null,DATABASE_VERSION);
+    }
+
+    @Override
+    public void onCreate(SQLiteDatabase db) {
+        String query = "CREATE TABLE if not EXISTS " +NOME_TABELA+
+                "("+
+                ID+ " INTEGER PRIMARY KEY, "+
+                CPF+" INTEGER, "+
+                RG+ " TEXT, "+
+                NOME+" TEXT, "+
+                STATUS+" TEXT "+
+                ")";
+        db.execSQL(query);
+    }
+
+    @Override
+    public void onUpgrade(SQLiteDatabase db, int oldVersion, int newVersion) {
+        db.execSQL("DROP TABLE IF EXISTS "+NOME_TABELA);
+    }
+
+    public void AddConvidado(ConvidadoModel convidado){
+        SQLiteDatabase db = this.getWritableDatabase();
+
+        ContentValues contentValues=new ContentValues();
+        contentValues.put(CPF, convidado.getCpf());
+        contentValues.put(RG, convidado.getRg());
+        contentValues.put(NOME, convidado.getNome());
+        contentValues.put(STATUS, convidado.getStatus());
+
+        db.insert(NOME_TABELA, null,contentValues);
+        db.close();
+    }
+
+    //GET CONVIDADO BY ID
+    public ConvidadoModel getConvidado(int id){
+        SQLiteDatabase db=this.getReadableDatabase();
+        Cursor cursor = db.query(
+                NOME_TABELA,
+                new String[]{ID,CPF,RG,NOME,STATUS},
+                ID+" = ?",
+                new String[]{String.valueOf(id)},
+                null,
+                null,
+                null);
+        if(cursor!=null){
+            cursor.moveToFirst();
+        }
+        ConvidadoModel convidadoModel = new ConvidadoModel(cursor.getString(0),cursor.getString(1),cursor.getString(2),cursor.getString(3), cursor.getString(4));
+        db.close();
+
+        return convidadoModel;
+    }
+
+    //GET LISTA DE CONVIDADOS
+    public List<ConvidadoModel> getAllConvidados(){
+        List<ConvidadoModel> convidadoModelList = new ArrayList<>();
+        String query = "SELECT * from "+NOME_TABELA;
+        SQLiteDatabase db = this.getReadableDatabase();
+        Cursor cursor = db.rawQuery(query,null);
+
+        if(cursor.moveToFirst()){
+            do{
+                ConvidadoModel convidadoModel = new ConvidadoModel(cursor.getString(0),cursor.getString(1),cursor.getString(2),cursor.getString(3), cursor.getString(4));
+                convidadoModelList.add(convidadoModel);
+            }while (cursor.moveToNext());
+        }
+        db.close();
+        return convidadoModelList;
+    }
+
+
+}
--- GateKeeper/app/src/main/java/com/example/gatekeeper/MainActivity.java
+++ GateKeeper/app/src/main/java/com/example/gatekeeper/MainActivity.java
@@ -6,14 +6,25 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.DialogInterface;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
 
+import com.example.gatekeeper.models.ConvidadoModel;
 import com.journeyapps.barcodescanner.ScanContract;
 import com.journeyapps.barcodescanner.ScanOptions;
+
+import org.w3c.dom.Text;
+
+import java.util.Date;
 
 public class MainActivity extends AppCompatActivity {
 
     Button btn_scan;
+    Button insert;
+    DatabaseHelper databaseHelper;
+    TextView datalist;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -21,10 +32,26 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
 
         btn_scan = findViewById(R.id.btn_scan);
-        btn_scan.setOnClickListener(v->
-                scanCode());
+        btn_scan.setOnClickListener(v-> scanCode());
+
+        databaseHelper = new DatabaseHelper(MainActivity.this);
+
+        // TA COM PAU AQUI-----RETORNANDO NULL
+        final Button insert = findViewById(R.id.btn_insert);
+        //-----
+
+        final TextView dataList = findViewById(R.id.btn_getall);
+
+        insert.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                ShowInputDialog();
+            }
+        });
+
     }
 
+    //METODO SCAN DO QRCODE
     private void scanCode() {
         ScanOptions options = new ScanOptions();
         options.setBeepEnabled(true);
@@ -47,5 +74,34 @@ public class MainActivity extends AppCompatActivity {
         }
     });
 
+    // TELA DO INSERT
+    private void ShowInputDialog() {
+        AlertDialog.Builder al = new AlertDialog.Builder(MainActivity.this);
+        View view = getLayoutInflater().inflate(R.layout.insert_dialog, null);
+        final EditText nome = view.findViewById(R.id.nome);
+        final EditText cpf = view.findViewById(R.id.cpf);
+        final EditText id = view.findViewById(R.id.id);
+        final EditText rg = view.findViewById(R.id.rg);
+        final EditText status = view.findViewById(R.id.status);
+        Button insertBtn = view.findViewById(R.id.btn_insert);
+        al.setView(view);
+
+        final AlertDialog alertDialog = al.show();
+
+        insertBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                ConvidadoModel convidadoModel = new ConvidadoModel("", "", "", "", "");
+                convidadoModel.setNome(nome.getText().toString());
+                convidadoModel.setCpf(cpf.getText().toString());
+                convidadoModel.setId(id.getText().toString());
+                convidadoModel.setStatus(status.getText().toString());
+                convidadoModel.setId(id.getText().toString());
+
+                databaseHelper.AddConvidado(convidadoModel);
+                alertDialog.dismiss();
+            }
+        });
+    }
 
 }
